e3041031139dca8c3519466f15be58d1c25c30c0
LUU HUYNH NGUYEN THI <luuhuynh.tkp@gmail.com>
2022-09-13 22:40:54 +0700

    js-buoi-7_8 [678910]

--- js/main.js
+++ js/main.js
@@ -106,3 +106,126 @@ btnTimSoChanCC.onclick = function () {
         txtSoChanCC.value = "Không có số chẵn nào trong mảng."
     }
 }
+
+// #6 Đổi chỗ
+var txtViTri1 = $("txtViTri1");
+var txtViTri2 = $("txtViTri2");
+var btnDoiCho = $("btnDoiCho");
+var txtDoiCho = $("txtDoiCho");
+
+function doiCho(arr, vitri1, vitri2) {
+    var temp = arr[vitri1];
+    arr[vitri1] = arr[vitri2];
+    arr[vitri2] = temp;
+    return arr;
+}
+
+btnDoiCho.onclick = function () {
+    var vitri1 = Number(txtViTri1.value);
+    var vitri2 = Number(txtViTri2.value);
+    arr = doiCho(arr, vitri1, vitri2);
+    txtDoiCho.value = "Mảng sau khi đổi chỗ:" + arr.join(", ");
+}
+
+// #7 Sắp xếp tăng dần
+
+var btnSapXep = $("btnSapXep");
+var txtSapXep = $("txtSapXep");
+
+btnSapXep.onclick = function () {
+    arr.sort();
+    txtSapXep.value = arr.join(", ");
+}
+
+// #8 Tìm số nguyên tố đầu tiên
+
+var btnTimSoNTDauTien = $("btnTimSoNTDauTien");
+var txtSoNTDauTien = $("txtSoNTDauTien");
+
+function laSoNguyenTo(n) {
+    if (n < 2) return false;
+    for (var i = 2; i < n; i++) {
+        if (n % i === 0) return false;
+    }
+    return true;
+}
+
+function soNguyenToDauTien(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (laSoNguyenTo(arr[i])) return arr[i];
+    }
+    return -1;
+}
+
+btnTimSoNTDauTien.onclick = function () {
+    var res = soNguyenToDauTien(arr);
+    if (res !== -1) {
+        txtSoNTDauTien.value = "Số nguyên tố đầu tiên: " + res;
+    }
+    else {
+        txtSoNTDauTien.value = "Không có số nguyên tố trong mảng.";
+    }
+}
+
+// #9 Đếm số nguyên
+
+var btnDemSoNguyen = $("btnDemSoNguyen");
+var txtDemSoNguyen = $("txtDemSoNguyen");
+
+function demSoNguyen(arr) {
+    var count = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === parseInt(arr[i]))
+            count++;
+    }
+    return count;
+}
+
+btnDemSoNguyen.onclick = function () {
+    var count = demSoNguyen(arr);
+    if (count) {
+        txtDemSoNguyen.value = "Có: " + count + " số nguyên trong mảng.";
+    }
+    else {
+        txtDemSoNguyen.value = "Không có số nguyên nào trogn mảng.";
+    }
+}
+
+// #10 So sánh số lượng âm và dương
+
+var txtSoN_ = $("txtSoN_");
+var btnThemSo_ = $("btnThemSo_");
+var txtMangSo_ = $("txtMangSo_");
+var btnSoSanh = $("btnSoSanh");
+var txtSoSanh = $("txtSoSanh");
+var myArr = [];
+
+btnThemSo_.onclick = function () {
+    var value = Number(txtSoN_.value);
+    myArr.push(value);
+    txtMangSo_.value = myArr.join(", ");
+    txtSoN_.value = "";
+    txtSoN_.focus();
+}
+
+function demChan(myArr) {
+    var count = 0;
+    for (var i = 0; i < myArr.length; i++) {
+        if (Math.abs(myArr[i]) % 2 === 0) count++;
+    }
+    return count;
+}
+
+btnSoSanh.onclick = function () {
+    var chan = demChan(myArr);
+    var le = myArr.length - chan;
+    if (chan > le) {
+        txtSoSanh.value = "Số chẵn > Số lẻ";
+    }
+    else if (chan === le) {
+        txtSoSanh.value = "Số chẵn = Số lẻ";
+    }
+    else {
+        txtSoSanh.value = "Số chẵn < Số lẻ";
+    }
+}
